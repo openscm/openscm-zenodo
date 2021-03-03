1b969fcbb0391e6a4e92f6219759ddfff228dd2d
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2021-03-03 16:49:16 +1100

    Format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -142,7 +142,9 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "openscm-zenodo", "OpenSCM-Zenodo Documentation", [author], 1)]
+man_pages = [
+    (master_doc, "openscm-zenodo", "OpenSCM-Zenodo Documentation", [author], 1)
+]
 
 # -- Options for Texinfo output ----------------------------------------------
 
