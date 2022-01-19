2750bdd93bea609c5379415f7ed2762c8787ab8e
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2022-01-19 17:05:17 +1100

    Format

--- tests/unit/test_cli_unit.py
+++ tests/unit/test_cli_unit.py
@@ -12,15 +12,7 @@ def test_upload_defaults(caplog, test_data_dir):
     tfile = os.path.join(test_data_dir, "test-deposit-metadata.json")
     tbucket = "bucket"
     with patch("openscm_zenodo.cli.upload_file") as mock_upload:
-        result = runner.invoke(
-            cli,
-            [
-                "upload",
-                tfile,
-                tbucket,
-            ],
-        )
-
+        result = runner.invoke(cli, ["upload", tfile, tbucket,],)
 
     assert not result.exit_code, result.stderr
     mock_upload.assert_called_with(
@@ -40,16 +32,8 @@ def test_upload_root_dir(caplog, test_data_dir):
     troot_dir = "path/to/somewhere"
     with patch("openscm_zenodo.cli.upload_file") as mock_upload:
         result = runner.invoke(
-            cli,
-            [
-                "upload",
-                tfile,
-                tbucket,
-                "--root-dir",
-                troot_dir
-            ],
+            cli, ["upload", tfile, tbucket, "--root-dir", troot_dir],
         )
-
 
     assert not result.exit_code, result.stderr
     mock_upload.assert_called_with(
--- tests/unit/test_zenodo_unit.py
+++ tests/unit/test_zenodo_unit.py
@@ -6,13 +6,24 @@ import pytest
 from openscm_zenodo.zenodo import upload_file
 
 
-@pytest.mark.parametrize("tfilepath,root_dir,exp_upload_path", (
-    (os.path.join("/path", "to", "file.txt"), None, "file.txt"),
-    # without trailing separator on root dir
-    (os.path.join("/path", "to", "file.txt"), os.path.join("/path"), os.path.join("to", "file.txt")),
-    # with trailing separator on root dir
-    (os.path.join("/path", "to", "file.txt"), os.path.join("/path/"), os.path.join("to", "file.txt")),
-))
+@pytest.mark.parametrize(
+    "tfilepath,root_dir,exp_upload_path",
+    (
+        (os.path.join("/path", "to", "file.txt"), None, "file.txt"),
+        # without trailing separator on root dir
+        (
+            os.path.join("/path", "to", "file.txt"),
+            os.path.join("/path"),
+            os.path.join("to", "file.txt"),
+        ),
+        # with trailing separator on root dir
+        (
+            os.path.join("/path", "to", "file.txt"),
+            os.path.join("/path/"),
+            os.path.join("to", "file.txt"),
+        ),
+    ),
+)
 def test_upload_file(tfilepath, root_dir, exp_upload_path):
     tbucket = "bucket"
     tzenodo_url = "zenodo.org"
@@ -27,16 +38,7 @@ def test_upload_file(tfilepath, root_dir, exp_upload_path):
         call_kwargs["root_dir"] = root_dir
 
     with patch("openscm_zenodo.zenodo.upload_with_progress_bar") as mock_upload:
-        res = upload_file(
-            tfilepath,
-            tbucket,
-            tzenodo_url,
-            ttoken,
-            **call_kwargs,
-        )
+        res = upload_file(tfilepath, tbucket, tzenodo_url, ttoken, **call_kwargs,)
 
     assert res is None
-    mock_upload.assert_called_with(
-        tfilepath,
-        expected_upload_url
-    )
+    mock_upload.assert_called_with(tfilepath, expected_upload_url)
