5050a6ec0dee2ba0458524e378eb9d52e673c6e4
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2024-09-26 16:53:39 +0200

    Clean up

--- tests/integration/test_cli_integration.py
+++ tests/integration/test_cli_integration.py
@@ -17,10 +17,3 @@ def test_version():
 
     assert result.exit_code == 0, result.exc_info
     assert result.stdout == f"openscm-zenodo {openscm_zenodo.__version__}\n"
-
-
-def test_say_hi():
-    result = runner.invoke(app, ["say-hi", "Tim"])
-
-    assert result.exit_code == 0, result.exc_info
-    assert result.stdout == "Hi Tim\n"
