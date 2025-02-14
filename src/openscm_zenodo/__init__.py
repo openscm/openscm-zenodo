5279fb95e64293d6a6c29cbff3556db45e1580a4
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2025-02-14 14:05:01 +0100

    Add top-level API and shuffle method order

--- src/openscm_zenodo/__init__.py
+++ src/openscm_zenodo/__init__.py
@@ -6,6 +6,24 @@ import importlib.metadata
 
 from loguru import logger
 
+from openscm_zenodo.zenodo import (
+    ZenodoDomain,
+    ZenodoInteractor,
+    create_new_version,
+    get_reserved_doi,
+    retrieve_bibtex_entry,
+    retrieve_metadata,
+)
+
 logger.disable("openscm_zenodo")
 
 __version__ = importlib.metadata.version("openscm_zenodo")
+
+__all__ = [
+    "ZenodoDomain",
+    "ZenodoInteractor",
+    "create_new_version",
+    "get_reserved_doi",
+    "retrieve_bibtex_entry",
+    "retrieve_metadata",
+]
