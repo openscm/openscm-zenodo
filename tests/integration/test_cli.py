addd32f5009216dbb1bce29953d09a958e8a2e87
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2025-08-09 23:15:53 +0200

    Bump typer used in lock file

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -14,7 +14,7 @@ from typer.testing import CliRunner
 from openscm_zenodo.cli.app import app
 from openscm_zenodo.zenodo import ZenodoDomain, ZenodoInteractor, retrieve_metadata
 
-runner = CliRunner(mix_stderr=False)
+runner = CliRunner()
 
 
 @pytest.mark.zenodo_token
